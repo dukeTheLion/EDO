a3321d3b55fbf18fef5710c1af2306c4b201d90b
Savio G. Mendonça (DUKE) <savio_gm@icloud.com>
2021-11-02 12:17:55 -0300

    Divisor

--- main.go
+++ main.go
@@ -5,19 +5,46 @@ import (
 	"math"
 )
 
-func factorial(n int)(result int) {
-	if n > 0 {
-		result = n * factorial(n-1)
-		return result
+var facts [40]int
+
+func factorial(n int) (res int) {
+	if facts[n] != 0 {
+		res = facts[n]
+		return res
 	}
+
+	if n > 0 {
+		res = n * factorial(n-1)
+		return res
+	}
+
 	return 1
+}
+
+func h(a float64, b float64, n int) float64 {
+	return (b - a) / float64(n)
+}
+
+func divisor(a float64, b float64, n int) []float64 {
+	aux := make([]float64, 0, n+1)
+	step := h(a, b, n)
+
+	aux = append(aux, a)
+
+	for i := 1; i < n; i++ {
+		aux = append(aux, a+step*float64(i))
+	}
+
+	aux = append(aux, b)
+
+	return aux
 }
 
 func taylorSeries(x float64, xi float64, y ...float64) float64 {
 	yn := y[0]
 
 	for i := 1; i < len(y); i++ {
-		aux := math.Pow(x - xi, float64(i))
+		aux := math.Pow(x-xi, float64(i))
 		aux /= float64(factorial(i))
 		aux *= y[i]
 
@@ -28,6 +55,15 @@ func taylorSeries(x float64, xi float64, y ...float64) float64 {
 }
 
 func main() {
-	fmt.Printf("%.40f", taylorSeries(0.5, 0, 1, 4, 13, 40, 119))
+	yn := []float64{1, 4, 13, 40, 119}
 
+	for i, v := range yn {
+		fmt.Print("y")
+		for j := 0; j < i; j++ {
+			fmt.Print("´")
+		}
+		fmt.Printf("(0) = %.0f     ", v)
+	}
+
+	fmt.Printf("\nf(0.5) = %.10f\n", taylorSeries(0.5, 0, yn...))
 }
